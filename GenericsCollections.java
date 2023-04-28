0a17ac9610c34c774302bacb9baf1974165acd1e
123nandha <nandhavilli123@gmail.com>
2023-04-28 12:03:53 +0530

    oop

---
+++ GenericsCollections.java
@@ -0,0 +1,22 @@
+package Oops;
+
+import java.util.TreeSet;
+
+public class GenericsCollections {
+
+	public static void main(String[] args) {
+       //int[] ar=new int[4];
+       //ar[0]=10;
+       //ar[1]=15;
+       //ar[2]=34;
+       //ar[3]=90;
+		//There is notbtype un collection
+		TreeSet ts=new TreeSet();
+		ts.add(10);
+		ts.add(5);
+		ts.add(50);
+		//ts.add("raja")(105); //tss
+		System.out.println(ts);//
+	}
+
+}
